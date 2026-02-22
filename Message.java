ffa311848b12c4a50bb58d98729bef4e6b276949
Killian Carolan <xriss_xross@outlook.com>
2026-02-22 13:05:09 +0000

    Trying a message object approach

---
+++ Message.java
@@ -0,0 +1,9 @@
+public class Message {
+	private int content;
+	private boolean elected;
+
+	public Message(int content, boolean elected) {
+		this.content = content;
+		this.elected = elected;
+	}
+}
