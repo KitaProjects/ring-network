b3809752d586f2f4dff1e8bf102582412954bef3
Killian Carolan <xriss_xross@outlook.com>
2026-02-23 13:33:59 +0000

    Fixed handling IDs logic

--- Node.java
+++ Node.java
@@ -1,11 +1,11 @@
 public class Node {
-	// Identity
+	// identity
 	private int myID;
 	private int wakeRound;
 	private String status;
 	private boolean terminate;
 
-	// Communication Links
+	// communication links
 	private Node nextNeighbour;
 	private int sendID;
 
@@ -29,20 +29,30 @@ public class Node {
 	}
 
 	public Message processMessage(int round, Message message) {
-		// If my ID has made it back to me I must be the leader
+		// returns null if asleep
+		if (wakeRound > round) {
+			return null;
+		}
+
+		// if my ID has made it back to me I must be the leader
 		if (message.content == this.myID) {
 			this.status = "leader";
 			this.terminate = true;
 			return new Message(this.myID, true);
-			// If another ID is larger than the one I want to send send that one instead
-		} else if (message.content > this.sendID) {
-			this.sendID = message.content;
-			// If someone has been elected and it's not me, I must be a follower
-		} else if (message.elected) {
-			this.terminate = true;
-			this.status = "follower";
 		}
 
-		return new Message(this.sendID, message.elected);
+		// if someone is elected who is not me, I must be a follower
+		if (message.elected) {
+			this.status = "follower";
+			this.terminate = true;
+			return new Message(message.content, true);
+		}
+
+		// handle when another ID is bigger than mine
+		if (message.content > this.sendID) {
+			this.sendID = message.content;
+		}
+
+		return new Message(this.sendID, false);
 	}
 }
